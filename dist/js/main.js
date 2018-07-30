4facac5269d93b48e62a1dbb51cc9fe6ec626b38
Sylwcuhaa <uzumagi1@gmail.com>
2018-07-30 19:30:27 +0200

    added some responsive for actual web progress layout and portrait img and style for this element

--- dist/js/main.js
+++ dist/js/main.js
@@ -24,7 +24,7 @@ function toggleMenu() {
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
-        menuNav.classList.remove('show')
+        menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
